42078bc439975c30a71565c3f041578c5656cbb3
milena-hoppe <milena.sales@aluno.senai.br>
2026-09-02 14:21:59 -0300

    Alteração do texto, cor do botão e variaveis

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -1,8 +1,14 @@
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Play } from "lucide-react-native"; 
 
-export default function Botao({ texto, onPress }) {
+export default function Botao({ texto, onPress, icone = false }) {
   return (
-    <TouchableOpacity style={styles.botao} onPress={onPress}>
+    <TouchableOpacity 
+      style={styles.botao} 
+      onPress={onPress}
+      activeOpacity={0.8} // Adicionado: controla a transparência ao clicar
+    >
+      {icone && <Play size={20} color="#FFFFFF" style={styles.icone} />}
       <Text style={styles.texto}>{texto}</Text>
     </TouchableOpacity>
   );
@@ -11,15 +17,25 @@ export default function Botao({ texto, onPress }) {
 const styles = StyleSheet.create({
   botao: {
     backgroundColor: "#E50914",
-    padding: 12,
+    padding: 14, 
     borderRadius: 8,
     alignItems: "center",
-    marginVertical: 5,
+    justifyContent: "center",
+    flexDirection: "row",
+    marginVertical: 8, 
+    elevation: 3, 
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
-
   texto: {
     color: "#FFFFFF",
     fontSize: 16,
     fontWeight: "bold",
+  },
+ 
+  icone: {
+    marginRight: 8, 
   },
 });
